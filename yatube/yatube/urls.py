dfb385b7c64598ab747c65d1d6a47ff013996ea9
Виталий Яремчук <absolut83@yandex.ru>
2022-01-25 19:01:47 +0300

    Готово 1

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import handler404, handler500
 
 handler404 = 'core.views.page_not_found'
 
@@ -13,7 +12,6 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
-
 
 
 if settings.DEBUG:
